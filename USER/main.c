1fd1dc90574b2dc3583bb2d0d7af33c6bf2b45f3
Hachikoko <18262381362@163.com>
2017-06-02 16:35:00 +0800

    在实现有线方案前，先保存一下

--- USER/main.c
+++ USER/main.c
@@ -9,12 +9,27 @@
 #include "stdlib.h"
 
 u8 node_index_for_base_station = 0;
-u8 node_index_for_end_node = 6;
+u8 node_index_for_end_node = 5;
 u8 extra_node_flag = 0;
 u8 current_frequency = 0;
 Link_Queue* link_queue = NULL;
 char ret_words[100];
 
+
+void supercore_init(void){
+	
+	//	Uart3_SendString((u8*)"AT+RST\r\n");  //设置模块速率为0；
+	//	delay_ms(10);
+	
+	Uart3_SendString((u8*)"AT+ODR=0\r\n");  //设置模块速率为0；
+	delay_ms(10);
+	Uart3_SendString((u8*)"AT+MODE=0\r\n");  //设置 1：9轴模式，0：6轴模式
+	delay_ms(10);
+	Uart3_SendString((u8*)"AT+DIR=0\r\n");  //设置 0：水平模式，1：竖直模式；
+	delay_ms(10);
+	Uart3_SendString((u8*)"AT+SETPTL=A0,B0,C0,D1\r\n");  //开启加速度、角速度、磁力计、四元数；
+	delay_ms(10);
+}
 
 int main(void)
 {
@@ -23,16 +38,7 @@ int main(void)
 	Uart1_init(115200);
 	Uart3_init(115200);
 	
-//	Uart3_SendString((u8*)"AT+RST\r\n");  //设置模块速率为0；
-//	delay_ms(10);
-	Uart3_SendString((u8*)"AT+ODR=0\r\n");  //设置模块速率为0；
-	delay_ms(10);
-	Uart3_SendString((u8*)"AT+MODE=0\r\n");  //设置6轴模式；
-	delay_ms(10);
-	Uart3_SendString((u8*)"AT+DIR=0\r\n");  //设置6轴模式；
-	delay_ms(10);
-	Uart3_SendString((u8*)"AT+SETPTL=A0,B0,C0,D1\r\n");  //开启加速度、角速度、磁力计、四元数；
-	delay_ms(10);
+	supercore_init();
 	
 	
 	NRF24L01_Init();
